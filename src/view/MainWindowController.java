fa6ab8265bd2e7d81b64242fd90a9c867501a66b
Alex <alex11535@bezeqint.net>
2018-12-13 04:17:10 +0200

    GUI ready!

--- src/view/MainWindowController.java
+++ src/view/MainWindowController.java
@@ -1,7 +1,11 @@
 package view;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 
 import java.io.File;
@@ -9,34 +13,38 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainWindowController implements Initializable {
-
-    int[][] mazeData = {{1,0,1,1,1,1,1,1,1,1,1,1},
-                        {1,1,0,1,1,1,1,1,1,1,1,1},
-                        {1,1,1,0,1,1,1,1,1,1,1,1},
-                        {1,1,1,0,1,1,1,1,1,1,1,1},
-                        {1,1,1,0,1,1,1,1,1,1,1,1},
-                        {1,1,1,0,1,1,1,1,1,1,1,1},
-                        {1,1,1,0,1,1,1,1,1,1,1,1},
-                        {1,1,1,0,0,0,0,0,1,1,1,1},
-                        {1,1,1,1,1,1,1,0,1,1,1,1},
-                        {1,1,1,1,1,1,1,0,0,0,0,0}
-    };
-
-
-
-    public void seteEnd(int rEnd) {
-        this.rEnd = rEnd;
-    }
-
-    int cStart;
-    int rEnd;
-
+    int[][] mazeData = { {1,1,0,0,1,1,1,1,1},
+                         {1,1,1,0,1,1,1,1,1},
+                         {1,1,1,0,1,1,1,1,1},
+                         {1,1,1,0,1,1,1,1,1},
+                         {1,1,1,0,0,1,1,1,1},
+                         {1,1,1,1,0,0,0,0,0},
+                         {1,1,1,1,1,1,1,1,0} };
     @FXML
     MazeDisplayer mazeDisplayer;
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         mazeDisplayer.setMazeData(mazeData);
+        mazeDisplayer.addEventFilter(MouseEvent.MOUSE_CLICKED, (e)-> mazeDisplayer.requestFocus() );
+        mazeDisplayer.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                int row = mazeDisplayer.getcRow();
+                int col = mazeDisplayer.getcCol();
+                if(event.getCode() == KeyCode.UP){
+                    mazeDisplayer.setPlayerPosition(row-1,col);
+                }
+                if(event.getCode() == KeyCode.DOWN){
+                    mazeDisplayer.setPlayerPosition(row+1,col);
+                }
+                if(event.getCode() == KeyCode.LEFT){
+                    mazeDisplayer.setPlayerPosition(row,col-1);
+                }
+                if(event.getCode() == KeyCode.RIGHT){
+                    mazeDisplayer.setPlayerPosition(row,col+1);
+                }
+            }
+        });
     }
     public void start(){
         System.out.println("Start Pipe game");
@@ -52,20 +60,5 @@ public class MainWindowController implements Initializable {
         }
     }
 
-    void setPlayer(int row,int col){
-        this.cStart = col;
-        this.rEnd = row;
-    }
-    public int getcStart() {
-        return cStart;
-    }
-
-    public void setcStart(int cStart) {
-        this.cStart = cStart;
-    }
-
-    public int geteEnd() {
-        return rEnd;
-    }
 
 }
--- src/view/MazeDisplayer.java
+++ src/view/MazeDisplayer.java
@@ -2,33 +2,56 @@ package view;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
 
 public class MazeDisplayer extends Canvas {
-    int[][] mazeData;
+    private int[][] mazeData;
+    private int cCol;
+    private int cRow;
 
     public void setMazeData(int[][] mazeData) {
         this.mazeData = mazeData;
+        cCol = 0;
+        cRow = 0;
+        redraw();
+    }
+
+    public void setPlayerPosition(int row,int col){
+        cCol = col;
+        cRow = row;
         redraw();
     }
 
     public void redraw(){
-        if(mazeData != null){
-           double width = getWidth();
-           double high = getHeight();
-           double w = width/mazeData[0].length;
-           double h = high/mazeData.length;
+        if(mazeData != null) {
+            double width = getWidth();
+            double high = getHeight();
+            double w = width / mazeData[0].length;
+            double h = high / mazeData.length;
 
             GraphicsContext graphicsContext = getGraphicsContext2D();
+            graphicsContext.clearRect(0,0,width,high);
+            for (int i = 0; i < mazeData.length; i++) {
+                for (int j = 0; j < mazeData[i].length; j++) {
+                    if (mazeData[i][j] != 0) {
+                        graphicsContext.fillRect(j * w, i * h, w, h);
 
-
-           for(int i= 0 ; i<mazeData.length ; i++){
-               for(int j = 0 ; j < mazeData[i].length ; j++){
-                   if(mazeData[i][j]!=0){
-                       graphicsContext.fillRect(j*w,i*h,w,h);
-                   }
-               }
-           }
+                    }
+                }
+            }
+            graphicsContext.setFill(Color.RED);
+            graphicsContext.fillOval(cCol * w, cRow * h, w, h);
 
         }
     }
+
+
+    public int getcCol() {
+        return cCol;
+    }
+
+    public int getcRow() {
+        return cRow;
+    }
+
 }
